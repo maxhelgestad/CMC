52c792c298583e7411312114ee149a675d5a101e
JKOST001 <jkost001@csbsju.edu>
2020-03-09 18:57:45 -0500

    updated criteria class

--- src/cmc/search/Criteria.java
+++ src/cmc/search/Criteria.java
@@ -2,6 +2,13 @@ package cmc.search;
 
 import java.util.List;
 
+
+/**
+ * Criteria class for use in the searching of universities
+ * 
+ * @author jkost001
+ *
+ */
 public class Criteria {
 
  
@@ -23,7 +30,22 @@ public class Criteria {
  private List<String> emphasis;
 
  /**
-  * @param location 
+  * criteria constructor
+  * 
+  * @param name school's name
+  * @param mfRatio male to female ratio
+  * @param numStudents the number of enrolled students
+  * @param control public or private control of the school
+  * @param satVerbal average verbal SAT stat for given school
+  * @param satMath average math SAT stat for school
+  * @param expenses cost of school
+  * @param financialAid the amount of financial aid offered by the school
+  * @param admitionRate the admission rate of the school
+  * @param percentEnrolled the percent of enrolled students
+  * @param academics 1-5 rating of academics at a school
+  * @param social 1-5 rating of school social life
+  * @param emphasis what the school is known for
+  * @param location the location of the school 
   * 
   */
  public Criteria(String name, String mfRatio, int numStudents, String control, int satVerbal,
@@ -45,5 +67,215 @@ public class Criteria {
   this.qualOfLife = qualOfLife;
   this.emphasis = emphasis;
  }
+
+/**
+ * @return the name
+ */
+public String getName() {
+	return name;
+}
+
+/**
+ * @param name the name to set
+ */
+public void setName(String name) {
+	this.name = name;
+}
+
+/**
+ * @return the mfRatio
+ */
+public String getMfRatio() {
+	return mfRatio;
+}
+
+/**
+ * @param mfRatio the mfRatio to set
+ */
+public void setMfRatio(String mfRatio) {
+	this.mfRatio = mfRatio;
+}
+
+/**
+ * @return the numStudents
+ */
+public int getNumStudents() {
+	return numStudents;
+}
+
+/**
+ * @param numStudents the numStudents to set
+ */
+public void setNumStudents(int numStudents) {
+	this.numStudents = numStudents;
+}
+
+/**
+ * @return the control
+ */
+public String getControl() {
+	return control;
+}
+
+/**
+ * @param control the control to set
+ */
+public void setControl(String control) {
+	this.control = control;
+}
+
+/**
+ * @return the location
+ */
+public String getLocation() {
+	return location;
+}
+
+/**
+ * @param location the location to set
+ */
+public void setLocation(String location) {
+	this.location = location;
+}
+
+/**
+ * @return the satVerbal
+ */
+public int getSatVerbal() {
+	return satVerbal;
+}
+
+/**
+ * @param satVerbal the satVerbal to set
+ */
+public void setSatVerbal(int satVerbal) {
+	this.satVerbal = satVerbal;
+}
+
+/**
+ * @return the satMath
+ */
+public int getSatMath() {
+	return satMath;
+}
+
+/**
+ * @param satMath the satMath to set
+ */
+public void setSatMath(int satMath) {
+	this.satMath = satMath;
+}
+
+/**
+ * @return the expenses
+ */
+public float getExpenses() {
+	return expenses;
+}
+
+/**
+ * @param expenses the expenses to set
+ */
+public void setExpenses(float expenses) {
+	this.expenses = expenses;
+}
+
+/**
+ * @return the financialAid
+ */
+public float getFinancialAid() {
+	return financialAid;
+}
+
+/**
+ * @param financialAid the financialAid to set
+ */
+public void setFinancialAid(float financialAid) {
+	this.financialAid = financialAid;
+}
+
+/**
+ * @return the admitionRate
+ */
+public float getAdmitionRate() {
+	return admitionRate;
+}
+
+/**
+ * @param admitionRate the admitionRate to set
+ */
+public void setAdmitionRate(float admitionRate) {
+	this.admitionRate = admitionRate;
+}
+
+/**
+ * @return the percentEnrolled
+ */
+public float getPercentEnrolled() {
+	return percentEnrolled;
+}
+
+/**
+ * @param percentEnrolled the percentEnrolled to set
+ */
+public void setPercentEnrolled(float percentEnrolled) {
+	this.percentEnrolled = percentEnrolled;
+}
+
+/**
+ * @return the academics
+ */
+public int getAcademics() {
+	return academics;
+}
+
+/**
+ * @param academics the academics to set
+ */
+public void setAcademics(int academics) {
+	this.academics = academics;
+}
+
+/**
+ * @return the social
+ */
+public int getSocial() {
+	return social;
+}
+
+/**
+ * @param social the social to set
+ */
+public void setSocial(int social) {
+	this.social = social;
+}
+
+/**
+ * @return the qualOfLife
+ */
+public int getQualOfLife() {
+	return qualOfLife;
+}
+
+/**
+ * @param qualOfLife the qualOfLife to set
+ */
+public void setQualOfLife(int qualOfLife) {
+	this.qualOfLife = qualOfLife;
+}
+
+/**
+ * @return the emphasis
+ */
+public List<String> getEmphasis() {
+	return emphasis;
+}
+
+/**
+ * @param emphasis the emphasis to set
+ */
+public void setEmphasis(List<String> emphasis) {
+	this.emphasis = emphasis;
+}
 
 }
