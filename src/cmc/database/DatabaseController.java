0b8a2a0bc0f4a23c56e55940ad85da0ec4ef71f1
JKOST001 <jkost001@csbsju.edu>
2020-03-10 12:01:05 -0500

    Added comments to univeristy controller and database controller classes

--- src/cmc/database/DatabaseController.java
+++ src/cmc/database/DatabaseController.java
@@ -9,12 +9,24 @@ import cmc.account.user.User;
 import cmc.search.Criteria;
 import cmc.university.University;
 import dblibrary.project.csci230.UniversityDBLibrary;
-
+/**
+ * A class to communicate with the database and relay the information to the rest of the 
+ * classes in the cmc website
+ * 
+ * @author Javengers
+ *
+ */
 public class DatabaseController {
 	private static UniversityDBLibrary lib;
 	private static Account noUser;
 	private static University noUniversity;
 
+	/**
+	 * Initializes the database and logs in with username and password
+	 * 
+	 * @param username our database username
+	 * @param password our database password
+	 */
 	public static void setUp(String username, String password) {
 		lib = new UniversityDBLibrary(username, password);
 		noUser = new Account("NoAccount", "x", "x", "x", 'u', 'y');
@@ -24,8 +36,7 @@ public class DatabaseController {
 	/**
 	 * get a user by the given name
 	 *
-	 * @param user
-	 *            name
+	 * @param username the user to retrieve from the database
 	 *
 	 * @reutrn a User with given user name
 	 */
@@ -46,15 +57,40 @@ public class DatabaseController {
 		return ac;
 	}
 
+	/**
+	 * Method for admin to edit an existing user, username cannot be changed
+	 * 
+	 * @param username the user to edit
+	 * @param newPassword the updated password for the user
+	 * @param newFirstName the updated first name for the user
+	 * @param newLastName the updated last name for the user
+	 * @param newType the updated type for the user
+	 * @param newStatus the updated status for the user
+	 */
 	public static void adminUserEdit(String username, String newPassword, String newFirstName, String newLastName,
 			char newType, char newStatus) {
 		lib.user_editUser(username, newFirstName, newLastName, newPassword, newType, newStatus);
 	}
 
+	/**
+	 * Method for admins to add new accounts to the system
+	 * 
+	 * @param firstName new account first name
+	 * @param lastName new account last name
+	 * @param username new account's username
+	 * @param password new account's password
+	 * @param type a for admin, u for user
+	 * @param status y for active, n for inactive
+	 */
 	public static void addAccount(String firstName, String lastName, String username, String password, char type, char status) {
 		lib.user_addUser(firstName, lastName, username, password, type);
 	}
 	
+	/**
+	 * Method for admins to remove accounts from the database
+	 * 
+	 * @param username the user to remove
+	 */
 	public static void removeAccount(String username) {
 		lib.user_deleteUser(username);
 	}
@@ -63,6 +99,12 @@ public class DatabaseController {
 		//TODO
 	//}
 
+	/**
+	 * A method to get the university object for a specific university name
+	 * 
+	 * @param name the name of the university to retrieve
+	 * @return the University object for the specified name
+	 */
 	public static University getUniversity(String name) {
 		ArrayList<University> schools = DatabaseController.getUniversities();
 		University un = noUniversity;
@@ -76,16 +118,33 @@ public class DatabaseController {
 		return un;
 	}
 
+	/**
+	 * Method to save a school to a user's saved schools in the database
+	 * 
+	 * @param username the user to save the school to
+	 * @param schoolName The school to save to the user
+	 */
 	public static void saveSchool(String username, String schoolName)
 	{
 		lib.user_saveSchool(username, schoolName);
 	}
 	
+	/**
+	 * A method to remove a specified school from a specified user's saved school list
+	 * 
+	 * @param username the user to remove the school from
+	 * @param schoolName the school to remove
+	 */
 	public static void removeSchool(String username, String schoolName)
 	{
 		lib.user_removeSchool(username, schoolName);
 	}
-
+	/**
+	 * A method to get all the saved schools for a specific user
+	 * 
+	 * @param username the user for which to get their saved school list
+	 * @return an ArrayList of universities that the specified user has saved
+	 */
 	public static ArrayList<University> getSavedSchools(String username){
 		  String[][] saved = lib.user_getUsernamesWithSavedSchools();
 		  ArrayList<University> result = new ArrayList<University>();
@@ -101,6 +160,11 @@ public class DatabaseController {
 		return result;
 	}
 
+	/**
+	 * Method to retrieve all of the universities in the database
+	 * 
+	 * @return an ArrayList of Universities 
+	 */
 	public static ArrayList<University> getUniversities() {
 		String[][] u = lib.university_getUniversities();
 		ArrayList<University> allSchools = new ArrayList<University>();
@@ -116,6 +180,11 @@ public class DatabaseController {
 		return allSchools;
 	}
 	
+	/**
+	 * Method to retrieve all the accounts from the database
+	 * 
+	 * @return an ArrayList of ACcounts 
+	 */
 	public static ArrayList<Account> getAccounts(){
 		String[][] a = lib.user_getUsers();
 		//System.out.println(a);
