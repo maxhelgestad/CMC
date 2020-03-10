bf5e27dc255a49dfb1753b8802b65c9cbc2fb035
vincentk21 <vkahlhame001@csbsju.edu>
2020-03-10 03:42:30 -0500

    Admininteraction JavaDoc

--- src/cmc/account/admin/AdminInteraction.java
+++ src/cmc/account/admin/AdminInteraction.java
@@ -8,27 +8,23 @@ import cmc.account.user.UserController;
 import cmc.university.University;
 import cmc.university.UniversityController;
 
-/**
- * 
- */
 
-/**
+/**Class to handel Admin Interaction
  * @author vkahlhame001
  *
  */
 public class AdminInteraction {
 
- /**
-  * 
-  */
- public AdminInteraction() {
-  // TODO Auto-generated constructor stub
- }
- 
+ /** Returns a list of the Universities
+ * @return List containing the universities
+ */
  public static ArrayList<University> viewUniversities() {
   return UniversityController.viewUniversities();
  }
  
+/** Returns a list of the Regular Users
+ * @return List containing the regular users
+ */
  public static ArrayList<Account> viewUsers() {
 	ArrayList<Account> users = new ArrayList<Account>();
 	ArrayList<Account> accounts = AccountController.getUsers();
@@ -41,6 +37,15 @@ public class AdminInteraction {
 	return users;
  }
  
+	
+/** Adds an account
+ * @param firstName of the account
+ * @param lastName of the account
+ * @param username of the account
+ * @param password of the account
+ * @param type of the account
+ * @param status of the account
+ */	
  public static void addAccount(String firstName, String lastName, String username, String password, char type, char status) {
 	AccountController.addAccount(firstName, lastName, username, password, type, status);
  }
@@ -49,13 +54,25 @@ public class AdminInteraction {
 	 //UserController.editUser(username, password, newUsername, newPassword); 
  //}
  
+	
+/** Deactivates an account
+ * @param username of the account
+ * @param password of the account
+ */
  public static void deactivateUser(String username, String password) {
 	 UserController.deactivateUser(username, password);
  }
 
+/** Edits an account
+ * @param firstName of the account
+ * @param lastName of the account
+ * @param username of the account
+ * @param password of the account
+ * @param type of the account
+ * @param status of the account
+ */
 public static void editProfile(String username, String newPassword, String newFirstName, String newLastName, char newType, char newStatus) {
-	AccountController.editProfile(username, newPassword,  newFirstName, newLastName, newType, newStatus);
-	
+	AccountController.editProfile(username, newPassword,  newFirstName, newLastName, newType, newStatus);	
 }
 
 }
