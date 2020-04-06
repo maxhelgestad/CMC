136723c05db1d1bef91ba7e1b94abadf771c9c36
MLI001 <mli001@vplincsci013.ad.csbsju.edu>
2020-04-06 17:29:03 -0500

    adding more test classes

---
+++ test/cmc/University/UniversityTest.java
@@ -0,0 +1,24 @@
+package cmc.University;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UniversityTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
