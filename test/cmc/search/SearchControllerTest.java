a6affb8270d94a71f207e93d990607a186bf7d37
VKAHLHAME001 <vkahlhame001@vplincsci053.ad.csbsju.edu>
2020-04-06 20:58:12 -0500

    fully working search tests

--- test/cmc/search/SearchControllerTest.java
+++ test/cmc/search/SearchControllerTest.java
@@ -23,7 +23,7 @@ public class SearchControllerTest {
 		DatabaseController.addUniversity("JACOB", "WISCONSIN", "RURAL", "PUBLIC", 5000, 50, 600, 600, 20000, 60, 40000, 50, 50, 3, 3, 2);
 		this.c1 = new Criteria("HELGEST", "FLOR", "", "P", 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000);
 		this.c2 = new Criteria("", "MINN", "URB", "P", 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000);
-		this.c3 = new Criteria("JACOB", "A", "RUR", "", 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000);
+		this.c3 = new Criteria("JACOB", "O", "RUR", "", 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000);
 		this.c4 = new Criteria("XYZ", "FLORIDQ", "RUR", "", -5, 50000, -1, 50000, -1, 50000, -1, 50000, -1, 50000, -1, 50000, -1, 50000, -1, 50000, -1, 50000, -1, 50000, -1, 50000, -1, 50000);
 		this.c5 = new Criteria("HELGESQ", "FLORIDQ", "", "PRI", -1, 50000, -1, 50000, -1, 50000, 1, 50000, 1, 50000, 1, 50000, 1, 50000, -1, 50000, 1, 50000, 1, 50000, -1, 50000, 1, 50000);
 		
@@ -45,7 +45,7 @@ public class SearchControllerTest {
 		//Black-Box Tests (Search)
 		Assert.assertTrue("Testing search with criteria 1", SearchController.searchSchool(this.c1).size() == 1);
 		Assert.assertTrue("Testing search with criteria 2", SearchController.searchSchool(this.c2).size() == 1);
-		//Assert.assertTrue("Testing search with criteria 3", SearchController.searchSchool(this.c3).size() == 1);
+		Assert.assertTrue("Testing search with criteria 3", SearchController.searchSchool(this.c3).size() == 1);
 		Assert.assertTrue("Testing search with criteria 4", SearchController.searchSchool(this.c4).size() == 0);
 		Assert.assertTrue("Testing search with criteria 5", SearchController.searchSchool(this.c5).size() == 0);
 		
