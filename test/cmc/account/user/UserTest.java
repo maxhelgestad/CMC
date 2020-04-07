e0734e9648d360b2ac057b7a0a143953e03d482b
BRICHARDS001 <brichards001@vplincsci058.ad.csbsju.edu>
2020-04-07 16:33:13 -0500

    completed the test case for remove school from saved school list

--- test/cmc/account/user/UserTest.java
+++ test/cmc/account/user/UserTest.java
@@ -51,4 +51,8 @@ public class UserTest {
 
 	}
 
+	@Test
+	public void testRemoveSchool() {
+		
+	}
 }
--- test/cmc/database/DatabaseControllerTest.java
+++ test/cmc/database/DatabaseControllerTest.java
@@ -72,5 +72,10 @@ public class DatabaseControllerTest {
 		Assert.assertTrue("Not a real name", DatabaseController.getUniversity("Not real university").getName().equals("NoUniversity"));
 		Assert.assertTrue("Blank String", DatabaseController.getUniversity("").getName().equals("NoUniversity"));
 	}
+	
+	@Test
+	public void testRemoveSchool() {
+		
+	}
 
 }
